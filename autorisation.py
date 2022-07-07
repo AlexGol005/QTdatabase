83db63eb7ce1c3403c5875b69f833c07d3069c28
AlexGol005 <sandra.005@mail.ru>
2022-07-07 20:02:34 +0300

    1

--- autorisation.py
+++ autorisation.py
@@ -17,15 +17,16 @@ class Ui_Autorisation(object):
     def setupUi(self, Autorisation):
         if not Autorisation.objectName():
             Autorisation.setObjectName(u"Autorisation")
-        Autorisation.resize(549, 475)
+        Autorisation.resize(599, 566)
         Autorisation.setStyleSheet(u"background-color: rgb(0, 97, 97);\n"
 "background-color: rgb(1, 96, 131);")
-        self.widget = QWidget(Autorisation)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(10, 10, 451, 431))
-        self.verticalLayout_2 = QVBoxLayout(self.widget)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.label_autorisation = QLabel(self.widget)
+        self.centralwidget = QWidget(Autorisation)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout_5 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.verticalLayout_7 = QVBoxLayout()
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.label_autorisation = QLabel(self.centralwidget)
         self.label_autorisation.setObjectName(u"label_autorisation")
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -37,55 +38,81 @@ class Ui_Autorisation(object):
 "color: rgb(255, 255, 255);")
         self.label_autorisation.setAlignment(Qt.AlignCenter)
 
-        self.verticalLayout_2.addWidget(self.label_autorisation)
+        self.verticalLayout_7.addWidget(self.label_autorisation)
 
-        self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.label_login = QLabel(self.widget)
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.label_login = QLabel(self.centralwidget)
         self.label_login.setObjectName(u"label_login")
         self.label_login.setStyleSheet(u"font: 75 18pt \"MS Shell Dlg 2\";\n"
 "color: rgb(255, 255, 255);")
         self.label_login.setAlignment(Qt.AlignCenter)
 
-        self.horizontalLayout.addWidget(self.label_login)
+        self.horizontalLayout_3.addWidget(self.label_login)
 
-        self.lineEdit_login = QLineEdit(self.widget)
+        self.lineEdit_login = QLineEdit(self.centralwidget)
         self.lineEdit_login.setObjectName(u"lineEdit_login")
         self.lineEdit_login.setStyleSheet(u"background-color: rgb(255, 255, 255);")
 
-        self.horizontalLayout.addWidget(self.lineEdit_login)
+        self.horizontalLayout_3.addWidget(self.lineEdit_login)
 
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.verticalLayout_7.addLayout(self.horizontalLayout_3)
 
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.label_password = QLabel(self.widget)
+
+        self.verticalLayout_5.addLayout(self.verticalLayout_7)
+
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_password = QLabel(self.centralwidget)
         self.label_password.setObjectName(u"label_password")
         self.label_password.setStyleSheet(u"font: 75 18pt \"MS Shell Dlg 2\";\n"
 "color: rgb(255, 255, 255);")
         self.label_password.setAlignment(Qt.AlignCenter)
 
-        self.horizontalLayout_2.addWidget(self.label_password)
+        self.horizontalLayout_5.addWidget(self.label_password)
 
-        self.lineEdit_password = QLineEdit(self.widget)
+        self.lineEdit_password = QLineEdit(self.centralwidget)
         self.lineEdit_password.setObjectName(u"lineEdit_password")
         self.lineEdit_password.setStyleSheet(u"background-color: rgb(255, 255, 255);")
 
-        self.horizontalLayout_2.addWidget(self.lineEdit_password)
+        self.horizontalLayout_5.addWidget(self.lineEdit_password)
 
 
-        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+        self.verticalLayout_5.addLayout(self.horizontalLayout_5)
 
-        self.pushButton_enter = QPushButton(self.widget)
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.label_no_acc = QLabel(self.centralwidget)
+        self.label_no_acc.setObjectName(u"label_no_acc")
+        self.label_no_acc.setStyleSheet(u"font: 75 12pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+
+        self.horizontalLayout_6.addWidget(self.label_no_acc)
+
+        self.pushButton_signup = QPushButton(self.centralwidget)
+        self.pushButton_signup.setObjectName(u"pushButton_signup")
+        self.pushButton_signup.setStyleSheet(u"background-color: rgb(0, 0, 255);\n"
+"font: 9pt \"MS Shell Dlg 2\"; color: rgb(255, 255, 255)")
+
+        self.horizontalLayout_6.addWidget(self.pushButton_signup)
+
+
+        self.verticalLayout_5.addLayout(self.horizontalLayout_6)
+
+        self.pushButton_enter = QPushButton(self.centralwidget)
         self.pushButton_enter.setObjectName(u"pushButton_enter")
         self.pushButton_enter.setMinimumSize(QSize(75, 0))
         self.pushButton_enter.setStyleSheet(u"background-color: rgb(0, 0, 255);\n"
 "font: 14pt \"MS Shell Dlg 2\"; color: rgb(255, 255, 255)\n"
 "")
 
-        self.verticalLayout_2.addWidget(self.pushButton_enter)
+        self.verticalLayout_5.addWidget(self.pushButton_enter)
 
+        Autorisation.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(Autorisation)
+        self.statusbar.setObjectName(u"statusbar")
+        Autorisation.setStatusBar(self.statusbar)
 
         self.retranslateUi(Autorisation)
 
@@ -93,10 +120,12 @@ class Ui_Autorisation(object):
     # setupUi
 
     def retranslateUi(self, Autorisation):
-        Autorisation.setWindowTitle(QCoreApplication.translate("Autorisation", u"\u0424\u043e\u0440\u043c\u0430 \u0430\u0432\u0442\u043e\u0440\u0438\u0437\u0430\u0446\u0438\u0438", None))
+        Autorisation.setWindowTitle(QCoreApplication.translate("Autorisation", u"\u0412\u0416-\u041f\u0410", None))
         self.label_autorisation.setText(QCoreApplication.translate("Autorisation", u"\u0410\u0432\u0442\u043e\u0440\u0438\u0437\u0430\u0446\u0438\u044f", None))
         self.label_login.setText(QCoreApplication.translate("Autorisation", u"\u041b\u043e\u0433\u0438\u043d", None))
         self.label_password.setText(QCoreApplication.translate("Autorisation", u"\u041f\u0430\u0440\u043e\u043b\u044c", None))
+        self.label_no_acc.setText(QCoreApplication.translate("Autorisation", u"   \u041d\u0435\u0442 \u0430\u043a\u043a\u0430\u0443\u043d\u0442\u0430?", None))
+        self.pushButton_signup.setText(QCoreApplication.translate("Autorisation", u"\u0417\u0430\u0440\u0435\u0433\u0438\u0441\u0442\u0438\u0440\u043e\u0432\u0430\u0442\u044c\u0441\u044f", None))
         self.pushButton_enter.setText(QCoreApplication.translate("Autorisation", u"\u0412\u043e\u0439\u0442\u0438", None))
     # retranslateUi
 
--- back.py
+++ back.py
@@ -5,6 +5,7 @@ from PySide2 import QtWidgets
 
 import sqlite3
 from design import Ui_MainWindow
+from notf import Ui_Notfound
 
 dirname = os.path.dirname(PySide2.__file__)
 plugin_path = os.path.join(dirname, 'plugins', 'platforms')
@@ -22,6 +23,12 @@ os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 #         # Создать объект курсора
 #         self.cur = self.con.cursor()
 
+class Notf(QtWidgets.QMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Notfound()
+        self.ui.setupUi(self)
 
 class MyWindow(QtWidgets.QMainWindow):
 
@@ -48,25 +55,31 @@ class MyWindow(QtWidgets.QMainWindow):
         lot = self.ui.lineEdit_lineEdit_search_kinematicviscosity_lot.text()
         temperature = self.ui.lineEdit_search_kinematicviscosity_temperature.text()
 
-        self.cur.execute(f"SELECT name, lot, temperature, certifiedValue FROM kinematicviscosity_viscositymjl "
-                         f"WHERE name='{name}' and lot='{lot}' and temperature='{temperature}'")
-        data = self.cur.fetchall()
 
-        r_count = len(data)
-        c_count = len(data[0])
+        try:
+            self.cur.execute(f"SELECT name, lot, temperature, certifiedValue FROM kinematicviscosity_viscositymjl "
+                             f"WHERE name='{name}' and lot='{lot}' and temperature='{temperature}'")
+            data = self.cur.fetchall()
 
-        table = self.ui.tableWidget_kinematic_results
-        table.setRowCount(r_count)
-        table.setColumnCount(c_count)
-        table.setHorizontalHeaderLabels(['Название стандартного образца', 'Партия', 'Температура,'
-                                        ' °C', 'Кинематическая вязкость, сСт'])
-        table.resizeColumnsToContents()
-        table.resizeRowsToContents()
+            r_count = len(data)
+            c_count = len(data[0])
 
-        for r in range(r_count):
-            for c in range(c_count):
-                cell = QTableWidgetItem(str(data[r][c]))
-                table.setItem(r, c, cell)
+            table = self.ui.tableWidget_kinematic_results
+            table.setRowCount(r_count)
+            table.setColumnCount(c_count)
+            table.setHorizontalHeaderLabels(['Название стандартного образца', 'Партия', 'Температура,'
+                                            ' °C', 'Кинематическая вязкость, сСт'])
+            table.resizeColumnsToContents()
+            table.resizeRowsToContents()
+
+            for r in range(r_count):
+                for c in range(c_count):
+                    cell = QTableWidgetItem(str(data[r][c]))
+                    table.setItem(r, c, cell)
+        except:
+            print('запись не найдена')
+            self.w2 = Notf()
+            self.w2.show()
 
 
 
